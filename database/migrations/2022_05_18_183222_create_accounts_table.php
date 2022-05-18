913219dc4dfb0d582e9dcea910ea80aff0e36b57
Giu1 <36491987+Giu1@users.noreply.github.com>
2022-05-19 00:04:16 +0100

    mysql + migration

--- database/migrations/2022_05_18_183222_create_accounts_table.php
+++ database/migrations/2022_05_18_183222_create_accounts_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('password');
+            $table->boolean('is_admin');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('accounts');
     }
 };
