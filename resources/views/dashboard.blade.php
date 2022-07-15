ed13255c0d1944ddd2c68bdc1f49076abf6f3f06
FORKLIFTDRIVER67 <2191192@my.ipleiria.pt>
2022-07-15 04:47:49 +0100

    Adapted header to a mobile screen

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,6 +31,6 @@
   <body>
     <div id="app"></div>
     <script src="{{ mix('js/app.js') }}"></script>
-    <script src="https://unpkg.com/@teleporthq/teleport-custom-scripts"></script>
+    <!--<script src="https://unpkg.com/@teleporthq/teleport-custom-scripts"></script>-->
   </body>
 </html>
